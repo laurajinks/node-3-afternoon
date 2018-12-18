940d0022a9dbf54709e204912c29732b88ccaa16
Laura <laura.jinks@gmail.com>
2018-12-18 16:58:39 -0600

    Fix search

--- server/controllers/search_controller.js
+++ server/controllers/search_controller.js
@@ -2,13 +2,13 @@ const swag = require('../models/swag');
 
 const search = (req, res, next) => {
     const {session} = req;
-    const {search} = req.body;
-    const category = swag.find(item => item.category === search)
-    if (category) {
+    const category = req.query.category;
+    // console.log(category)
+    if (!category) {
+        res.status(200).json(swag);
+    } else {
         const arr = swag.filter(item => item.category === category)
         res.status(200).json(arr);
-    } else {
-        res.status(200).json(swag);
     }
 }
 
